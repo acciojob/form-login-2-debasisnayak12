8c52ff3bac8bbeb33b87631a53b826ed50768cad
Debasis Nayak <115566899+debasisnayak12@users.noreply.github.com>
2023-11-11 10:47:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+function myFunction(){
+	 let firstNameValue = document.getElementById("firstName").value;
+	 let lastNameValue = document.getElementById("lastName").value;
+	 let phoneNumberValue = document.getElementById("phoneNumber").value;
+	 let emailValue = document.getElementById("email").value;
+	 
+
+      var resultMessage = "First Name:" + firstNameValue + "\n" +
+		                    "Last Name:" + lastNameValue + "\n" +
+		                    "Phone Number:" + phoneNumberValue + "\n" +
+		                    "Email ID:" + emailValue;
+		              
+      alert(resultMessage);
+}
